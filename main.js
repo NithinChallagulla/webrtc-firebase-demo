c258467e8675b8b5b9740f7c3a89e1fad154d5f8
Nithin Challagulla <115912319+NithinChallagulla@users.noreply.github.com>
2025-05-11 23:51:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ import 'firebase/firestore';
 
 const firebaseConfig = {
   // your config
+   apiKey: "AIzaSyDooJlQCfk104yS4XzOj1bgpVEoOB8rTnQ",
+  authDomain: "webrtc-demo-33437.firebaseapp.com",
+  projectId: "webrtc-demo-33437",
+  storageBucket: "webrtc-demo-33437.firebasestorage.app",
+  messagingSenderId: "99021841417",
+  appId: "1:99021841417:web:510b0562a0d20c437ec4d8",
+  measurementId: "G-H2KFF0EHS0"
 };
 
 if (!firebase.apps.length) {
